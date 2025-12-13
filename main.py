0741121dc353fb260b66adc2934d219fec6c4faf
Okwus <xumn03@hotmail.com>
2025-12-13 13:45:11 -0300

    Added more commands.

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 def on_on_chat():
-    player.say(agent.get_orientation())
-player.on_chat("agent get {dir}", on_on_chat)
+    agent.set_assist(PLACE_ON_MOVE, True)
+player.on_chat("agent defprop PLACE_ON_MOVE true", on_on_chat)
 
 def on_on_chat2():
-    player.say(agent.get_position())
-player.on_chat("agent get {pos}", on_on_chat2)
+    for index in range(100):
+        mobs.spawn(CHICKEN, pos(0, 10, 0))
+player.on_chat("chicken", on_on_chat2)
 
 def on_on_chat3():
-    agent.teleport_to_player()
-player.on_chat("?follow {me}", on_on_chat3)
+    loops.pause(100)
+    player.say(agent.get_position())
+player.on_chat("agent get position", on_on_chat3)
 
-agent.teleport_to_player()
+def on_on_chat4():
+    loops.pause(100)
+    agent.teleport_to_player()
+    agent.move(FORWARD, 5)
+player.on_chat("agent follow me", on_on_chat4)
+
+def on_on_chat5():
+    player.say("Lista de comandos completa:")
+    loops.pause(100)
+    agent.teleport_to_player()
+    player.say("1: agent")
+    player.say("2: agent follow me")
+    player.say("3: agent get position")
+    player.say("4: agent defprop put-block-in-movement true")
+player.on_chat("agent help", on_on_chat5)
+
+# AgentTalk e seus afiliados são propriedade intelectual da Wave Technologies LTDA e da Microsoft Corporation e não podem ser vendidos sem a devida autorização.
+
+def on_on_chat6():
+    agent.teleport_to_player()
+    player.say("Digite \"agent help\" para uma lista completa de comandos do AgentTalk")
+player.on_chat("agent", on_on_chat6)
+
+"""
+
+©2026 Wave Technologies LTDA. Todos os direitos resevados.
+
+"""
+player.say("Digite \"agent help\" para uma lista completa de comandos do AgentTalk")
--- main.ts
+++ main.ts
@@ -1,13 +1,46 @@
+/**
+ * ©2026 Wave Technologies LTDA. Todos os direitos resevados.
+ * 
+ * AgentTalk e seus afiliados são propriedade intelectual da Wave Technologies LTDA e da Microsoft Corporation e não podem ser vendidos sem a devida autorização.
+ * 
+ * Este Add-On está sob desenvolvimento contínuo e não pode ser parado por não-autoridades.
+ */
+player.onChat("agent defprop PLACE_ON_MOVE true", function () {
+    agent.setAssist(PLACE_ON_MOVE, true)
+})
+player.onChat("chicken", function () {
+    for (let index = 0; index < 100; index++) {
+        mobs.spawn(CHICKEN, pos(0, 10, 0))
+    }
+})
 player.onChat("agent get position", function () {
     loops.pause(100)
     player.say(agent.getPosition())
 })
+events.onEndOfDay(function () {
+    agent.teleportToPlayer()
+    player.say("Contate \"agent help\" para ver o que você pode fazer para sobreviver")
+})
+events.onItemUsed(function (item, method) {
+	
+})
 player.onChat("agent follow me", function () {
     loops.pause(100)
     agent.teleportToPlayer()
+    agent.move(FORWARD, 5)
 })
+player.onChat("agent help", function () {
+    player.say("Lista de comandos completa:")
+    loops.pause(100)
+    agent.teleportToPlayer()
+    player.say("1: agent")
+    player.say("2: agent follow me")
+    player.say("3: agent get position")
+    player.say("4: agent defprop put-block-in-movement true")
+})
+// AgentTalk e seus afiliados são propriedade intelectual da Wave Technologies LTDA e da Microsoft Corporation e não podem ser vendidos sem a devida autorização.
 player.onChat("agent", function () {
     agent.teleportToPlayer()
     player.say("Digite \"agent help\" para uma lista completa de comandos do AgentTalk")
 })
-agent.teleportToPlayer()
+player.say("Digite \"agent help\" para uma lista completa de comandos do AgentTalk")
